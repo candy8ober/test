946ffbc949f30d00e2c7ddea9e9ad92f9b87bfbc
Gunel Aliyeva <samadovagunel@gmail.com>
2018-12-14 22:55:55 +0100

    hey yoo

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,10 +2,10 @@ package test;
 
 public class Test {
 	
-	private static final byte _byte = 'e';
+	private static final byte _byte = 'b';
 
 	public static void main(String[] args) {
-		System.out.println(_byte);
+		System.out.println(_byte + "hey yoo");
 	}
 
 }
